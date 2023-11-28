d0e6e319b8410f0c13ecf0d7e730e7962de0bd00
MaksBesklepny <132653865+MaksBesklepny@users.noreply.github.com>
2023-11-28 14:17:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from models.Climbers import Climbers
+from models.Model import Model
+climber = Climbers()
+climber.get()
+for row, list in enumerate(climber.get()):
+    print(row, ' - ', list)
+#climber = Model()
+#print(climber.get('Climbers'))
+
+climber.add()
+
+
+#climber.update()
+
+for row, list in enumerate(climber.get()):
+    print(row, ' - ', list)
---
+++ models/Model.py
@@ -0,0 +1,49 @@
+from configuration.config import connection
+
+
+class Model:
+    def get(self,table):
+        with connection().cursor() as cursor:
+            select_all_rows = f"SELECT * FROM {table}"
+            cursor.execute(select_all_rows)
+            return cursor.fetchall()
+        connection().close()
+
+    def get_oneStroke(self, table, id):
+        with connection().cursor() as cursor:
+            select_oneStroke = f"SELECT * FROM {table} WHERE id = {id}"
+            cursor.execute(select_oneStroke)
+            return cursor.fetchone()
+        connection().close()
+
+    def get_oneField(self, table, field):
+        with connection().cursor() as cursor:
+            select_oneField = f"SELECT {field} FROM {table}"
+            cursor.execute(select_oneField)
+            return cursor.fetchone()
+        connection().close()
+
+    def add(self, table, str, *values):
+        with connection().cursor() as cursor:
+            print(f"INSERT INTO {table} ({str}) VALUES ({values})")
+            query = f"INSERT INTO {table} ({str}) VALUES ({values})"
+            cursor.execute(query)
+            connection().close()
+            print(f"Новая запись в таблицу {table} добавлена")
+
+    def delete(self, table, id):
+        with connection().cursor() as cursor:
+            query_delete = f"DELETE FROM {table} WHERE id = {id}"
+            cursor.execute(query_delete)
+            connection().commit()
+            connection().close()
+            print("Запись удалена")
+
+    def update(self, table, id, field, values):
+        with connection().cursor() as cursor:
+            print(f"UPDATE {table} set {field} = '{values}' where id = {id}")
+            query_update = f"UPDATE {table} set {field} = '{values}' where id = {id}"
+            cursor.execute(query_update)
+            connection().commit()
+            connection().close()
+            print("Запись обновлена")
